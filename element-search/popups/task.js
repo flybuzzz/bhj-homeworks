a2e81141480002cfdb3cfb9fd172d31b18a19fe0
flybuzzz <111250257+flybuzzz@users.noreply.github.com>
2023-05-21 13:17:36 +0300

    Update task.js

---
+++ element-search/popups/task.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const modalMain = document.getElementById('modal_main');
+const modalCloses = document.querySelectorAll('.modal__close');
+const modal = document.getElementsByClassName('modal');
+const modalSuccess = document.getElementById('modal_success');
+const showSuccess = document.querySelector('.show-success');
+
+    modalMain.classList.add('modal_active');
+
+    for(let modalClose of modalCloses){ 
+
+    modalClose.onclick = () => {
+       modalMain.classList.remove('modal_active');
+       modalSuccess.classList.remove('modal_active');
+    }
+}
+
+    showSuccess.onclick = () => {
+        modalSuccess.classList.add('modal_active');
+        modalMain.classList.remove('modal_active');
+    }
