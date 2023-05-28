a01a47af19ef5402133938ebc00169acc4ac715f
flybuzzz <111250257+flybuzzz@users.noreply.github.com>
2023-05-28 19:31:35 +0300

    Update task

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,1 +1,16 @@
+const dropdown = document.querySelector('.dropdown');
+const list = document.querySelector('.dropdown__list');
+const value = document.querySelector('.dropdown__value');
 
+const link = Array.from(document.querySelectorAll('.dropdown__link'));
+
+dropdown.addEventListener('click', (event) => {
+	list.classList.toggle('dropdown__list_active');
+});
+
+for (let i = 0; i < link.length; i++) {
+	link[i].onclick = () => {
+		value.closest('div').textContent = link[i].textContent;
+		return false;
+	}
+}
